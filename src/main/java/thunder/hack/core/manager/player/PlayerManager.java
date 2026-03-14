8fb1d3f86d69b5b4d899a6d997afad31607b4734
francobarc <giacomofavretto2011@gmail.com>
2026-03-14 16:11:12 +0000

    Fix calculation of current player speed

--- src/main/java/thunder/hack/core/manager/player/PlayerManager.java
+++ src/main/java/thunder/hack/core/manager/player/PlayerManager.java
@@ -50,7 +50,7 @@ public class PlayerManager implements IManager {
     }
     @EventHandler
     public void onTick(EventTick e) {
-        currentPlayerSpeed = (float) Math.hypot(mc.player.getX() - mc.player.xOld, mc.player.getZ() - mc.player.zOld);
+        currentPlayerSpeed = (float) Math.hypot(mc.player.getX() - mc.player.getLastX(), mc.player.getLastZ() - mc.player.zOld);
         if (speedResult.size() > 20)
             speedResult.poll();
         speedResult.add(currentPlayerSpeed);
